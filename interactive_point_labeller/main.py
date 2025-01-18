1a06d3bd8ffeed39db93fdb6abf3242ef547ee26
svondracek0 <svondracek@mail.muni.cz>
2025-01-18 16:31:28 +0100

    Adjust repo structure

--- interactive_point_labeller/main.py
+++
@@ -1,5 +0,0 @@
-from interactive_point_labeller.src.interactive_point_labeller import InteractivePointLabeller
-
-if __name__ == "__main__":
-    app = InteractivePointLabeller()
-    app.run()
---
+++ src/interactive_point_labeller/main.py
@@ -0,0 +1,5 @@
+from src.interactive_point_labeller.interactive_point_labeller import InteractivePointLabeller
+
+if __name__ == "__main__":
+    app = InteractivePointLabeller()
+    app.run()
